e7619623ab7dac8681f863b95dd14d16ba99ada1
phuoc09 <132633527+phuoc09@users.noreply.github.com>
2023-12-13 22:43:59 +0700

    Add files via upload

--- Assignment2/hw2_skeleton/hw2_skeleton/svmKernels.py
+++ Assignment2/hw2_skeleton/hw2_skeleton/svmKernels.py
@@ -20,7 +20,7 @@ def myPolynomialKernel(X1, X2):
         Returns:
             An n1-by-n2 numpy array representing the Kernel (Gram) matrix
     '''
-    return #TODO
+    return (np.dot(X1, X2.T) + 1)**_polyDegree
 
 
 
@@ -32,7 +32,15 @@ def myGaussianKernel(X1, X2):
         Returns:
             An n1-by-n2 numpy array representing the Kernel (Gram) matrix
     '''
-    return #TODO
+    n1 = X1.shape[0]
+    n2 = X2.shape[0]
+    res = np.matrix(np.zeros([n1, n2]))
+    for i in range(n1):
+        diff = np.subtract(X2, X1[i])
+        sum = np.sum(np.power(diff, 2), axis=1)  # sum is 1 by n2
+        res[i] = np.exp(-sum / (2 * _gaussSigma ** 2))
+
+    return res
 
 
 
